63e9799edb5a8ca54e24d3643839fa1f0f278996
ngicks <yknt.bsl@gmail.com>
2023-08-11 17:57:01 +0900

    prevent image file from overwriting volume content

---
+++ create_volume.sh
@@ -0,0 +1,7 @@
+docker volume create git
+docker volume create vscode-server
+docker volume create deno
+docker volume create npm
+docker volume create go
+docker volume create cargo
+docker volume create rustup
